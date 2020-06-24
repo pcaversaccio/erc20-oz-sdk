c3d75d3a50ceed9e4e8f7223c6c3150b9a3e9ddc
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2020-06-24 14:29:23 +0200

    Added upgraded TokenExchange smart contract.

---
+++ contracts/TokenExchange_2.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.9;
+
+// Import base Initializable contract
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+
+// Import the IERC20 interface and and SafeMath library
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+
+
+contract TokenExchange is Initializable {
+    using SafeMath for uint256;
+
+    // Contract state: exchange rate and token
+    uint256 public rate;
+    IERC20 public token;
+
+    // Set owner
+    address public owner;
+
+    function withdraw() public {
+        require(
+            msg.sender == owner,
+            "Address not allowed to call this function."
+        );
+        msg.sender.transfer(address(this).balance);
+    }
+
+    // Initializer function (replaces constructor)
+    function initialize(uint256 _rate, IERC20 _token) public initializer {
+        rate = _rate;
+        token = _token;
+    }
+
+    // Send tokens back to the sender using predefined exchange rate
+    receive() external payable {
+        uint256 tokens = msg.value.mul(rate);
+        token.transfer(msg.sender, tokens);
+    }
+
+    // To be run during upgrade, ensuring it can never be called again
+    function setOwner(address _owner) public {
+        require(owner == address(0), "Owner already set, cannot modify!");
+        owner = _owner;
+    }
+
+}
